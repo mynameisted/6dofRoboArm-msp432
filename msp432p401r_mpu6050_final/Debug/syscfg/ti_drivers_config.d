053b3ae9e17edd723424f4406ae78bcf664f5d7f
Ted Lai <42962723+mynameisted@users.noreply.github.com>
2019-11-29 13:39:13 +0800

    Added MPU6050 final code

---
+++ msp432p401r_mpu6050_final/Debug/syscfg/ti_drivers_config.d
@@ -0,0 +1,75 @@
+# FIXED
+
+syscfg/ti_drivers_config.obj: syscfg/ti_drivers_config.c
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/DeviceFamily.h
+syscfg/ti_drivers_config.obj: syscfg/ti_drivers_config.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/I2C.h
+syscfg/ti_drivers_config.obj: C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/i2c/I2CMSP432.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/HwiP.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/SemaphoreP.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/Power.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/utils/List.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp432p401r.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp_compatibility.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/core_cm4.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_version.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_compiler.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_ccs.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/mpu_armv7.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/interrupt.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/i2c.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/eusci.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/power/PowerMSP432.h
+syscfg/ti_drivers_config.obj: C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/Board.h
+
+syscfg/ti_drivers_config.c: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stddef.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/DeviceFamily.h: 
+syscfg/ti_drivers_config.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/I2C.h: 
+C:/ti/ccs900/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/i2c/I2CMSP432.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/HwiP.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/dpl/SemaphoreP.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/Power.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/utils/List.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp432p401r.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp_compatibility.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/msp432p401r_classic.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/core_cm4.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_version.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_compiler.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/cmsis_ccs.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/third_party/CMSIS/Include/mpu_armv7.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/inc/system_msp432p401r.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/interrupt.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/i2c.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/devices/msp432p4xx/driverlib/eusci.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/power/PowerMSP432.h: 
+C:/ti/simplelink_msp432p4_sdk_3_30_00_13/source/ti/drivers/Board.h: 
